ba0c71691491cac0e08a01353de2e83f588c9542
Anagh Patel <anagh0106@gmail.com>
2024-10-02 18:05:35 +0530

    First

---
+++ Basics/First.py
@@ -0,0 +1,11 @@
+print("Hello Guys")
+
+# This is comment
+
+"""
+This is not multiline Comment but it is doc-string
+"""
+
+# used to separate strings
+# print("Hello","Guys",sep="-")
+print("Hii","I am","Naman",sep='\'')
