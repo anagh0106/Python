413b0564da261b7e771ccb0cf08412cb0f559b85
Anagh Patel <anagh0106@gmail.com>
2024-10-13 23:44:25 +0530

    String Method

---
+++ Basics/StringMethods.py
@@ -0,0 +1,24 @@
+# string="Hello Guys I am Your Frnd"
+# text = "24_Club_Python_GNR"
+# print(string.count('o'))
+# print(string.casefold()=="Hello GUys") # return boolean
+# print(string.casefold().count("o"))
+# print(string.count("o",0,6))
+# print(len(string))
+# print(text.center(15,'-'))
+# print(text.upper())
+# print("!9ε@Ä93".encode(encoding='ascii', errors='replace'))
+
+text = "Good {}!"
+# print(text.format("Evening"))
+# print("Addition of {} and {} is {}".format(5, 3, (5+3)))
+# print("Subtraction of {0} and -{1} is {2}".format(5, 5+3, 3))
+# print(f"Addition {5} and {3} is {5+3}")
+
+# print("hello".isalpha())
+# print("1he2llo12".isalnum())
+# print("1he2llo12".isascii())
+# print("royal technosoft".partition("tech"))
+# print("royal technosoft".partition(" "))
+
+print("11001".zfill(8))
