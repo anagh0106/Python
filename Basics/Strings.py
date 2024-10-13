ba157ebd2b2b88a9ac0f6faca5d9315b43c0664b
Anagh Patel <anagh0106@gmail.com>
2024-10-13 23:43:49 +0530

    Strings

---
+++ Basics/Strings.py
@@ -0,0 +1,20 @@
+string="The quick brown fox jumps over the lazy dog."
+
+#  reverse a substring from index -10 to -30 with a step of -1
+# s1=string[-30:-10]
+# print(s1[::-1])
+
+#  Extract "dog lazy the over" form the reversed string
+# rev=string[::-1]
+# print(rev[0:18])
+
+# Extract every second character in reverse from the original string
+# rev1=string[0::2]
+# print(rev1[::-1])
+
+"""Formated String => f{.....}"""
+
+name='Deep'
+print(f"{name} , Good Morning")
+
+# print(string[-9:])
