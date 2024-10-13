c017eb630c0d4bd269df836909331c36b775020b
Anagh Patel <anagh0106@gmail.com>
2024-10-13 23:43:30 +0530

    First

--- Basics/First.py
+++ Basics/First.py
@@ -1,5 +1,4 @@
-print("Hello Guys")
-
+# print("Hello Guys")
 # This is comment
 
 """
@@ -8,4 +7,32 @@ This is not multiline Comment but it is doc-string
 
 # used to separate strings
 # print("Hello","Guys",sep="-")
-print("Hii","I am","Naman",sep='\'')
+# print("Hii","I am","Naman",sep='\'')
+# print("Apple","Banana","Orange",sep=",")
+# print("\"Hello World\"") # Print with double quotes
+
+"""Data Types"""
+
+# There are following data types in python
+
+"""
+
+1. Text: str
+2. Numeric: int, float, complex(117j)
+3. Sequence: list, tuple, range
+4. Mapping: dict
+5. Set: set, frozenset
+6. Boolean: bool
+7. Binary: bytes(binary), bytearray(array of binary nums)
+8. None: NoneType
+
+"""
+
+# id()= used to verify identity of variable
+# type()= used to check the type of data and return in class form
+
+# a="str"
+# b="str1"
+# print(id(a),id(b))
+# print(type(a))
+
